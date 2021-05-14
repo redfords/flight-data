2846b84828c960d19d9675f3dced3fff562570c3
redfords <joanapiovaroli@gmail.com>
2021-05-14 01:33:18 -0300

    Remove index from df

--- insert_tables.py
+++ insert_tables.py
@@ -11,25 +11,27 @@ def extract_from_csv(file_to_process):
 
     return dataframe
 
+print("Uploading into database...")
+
 # insert country data into MySQL
 data = extract_from_csv('country.csv')
 data = data.iloc[:, 1:]
-data.to_sql('country', con = engine, if_exists = 'append', index_label = 'country_id', chunksize = 100)
+data.to_sql('country', con = engine, if_exists = 'append', index = False, chunksize = 100)
 
 # insert city data into MySQL
 data = extract_from_csv('city.csv')
 data = data.iloc[:, 1:]
-data.to_sql('city', con = engine, if_exists = 'append', index_label = 'city_id', chunksize = 100)
-
-# insert airline data into MySQL
-data = extract_from_csv('airline.csv')
-data = data.iloc[:, 1:]
-data.to_sql('airline', con = engine, if_exists = 'append', index_label = 'airline_id', chunksize = 100)
+data.to_sql('city', con = engine, if_exists = 'append', index = False, chunksize = 100)
 
 # insert airport data into MySQL
 data = extract_from_csv('airport.csv')
 data = data.iloc[:, 1:]
-data.to_sql('airport', con = engine, if_exists = 'append', index_label = 'airport_id', chunksize = 100)
+data.to_sql('airport', con = engine, if_exists = 'append', index = False, chunksize = 100)
+
+# insert airline data into MySQL
+data = extract_from_csv('airline.csv')
+data = data.iloc[:, 1:]
+data.to_sql('airline', con = engine, if_exists = 'append', index = False, chunksize = 100)
 
 # insert flight data into MySQL
 data = extract_from_csv('flight.csv')
