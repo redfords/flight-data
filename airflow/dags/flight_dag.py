0460d4b940cec67aee0cc2de059250b6ad01d035
redfords <joanapiovaroli@gmail.com>
2021-05-18 20:52:08 -0300

    Update load_data()

--- airflow/dags/flight_dag.py
+++ airflow/dags/flight_dag.py
@@ -8,55 +8,18 @@ import pandas as pd
 def load_data():
 	mysql_hook = MySqlHook(mysql_conn_id = 'flight_id')
 
-	table_name = 'flight'
+	table_name = ['country', 'city', 'airport', 'airline', 'flight']
 
-	# convert .csv into dataframe
-	data = pd.read_csv('~/airflow/dags/files/' + table_name + '.csv')
-	data = data.iloc[:, 1:]
+	for table in table_name:
+		# convert .csv into dataframe
+		data = pd.read_csv('~/airflow/dags/files/' + table_name + '.csv')
+		data = data.iloc[:, 1:]
 
-	# convert dataframe into list of tuples
-	rows = list(data.itertuples(index = False, name = None))
+		# convert dataframe into list of tuples
+		rows = list(data.itertuples(index = False, name = None))
 
-	# insert list of tuples into db
-	mysql_hook.insert_rows(
-		table = table_name,
-		rows = rows,
-		target_fields = [
-			'flight_date',
-			'flight_status',
-			'departure_airport',
-			'departure_timezone',
-			'departure_iata',
-			'departure_icao',
-			'departure_terminal',
-			'departure_gate',
-			'departure_delay',
-			'departure_scheduled',
-			'departure_estimated',
-			'departure_actual',
-			'departure_estimated_runway',
-			'departure_actual_runway',
-			'arrival_airport',
-			'arrival_timezone',
-			'arrival_iata',
-			'arrival_icao',
-			'arrival_terminal',
-			'arrival_gate',
-			'arrival_baggage',
-			'arrival_delay',
-			'arrival_scheduled',
-			'arrival_estimated',
-			'arrival_actual',
-			'arrival_estimated_runway',
-			'arrival_actual_runway',
-			'airline_name', 
-			'airline_iata',
-			'airline_icao',
-			'flight_number',
-			'flight_iata',
-			'flight_icao'
-            ]
-		)
+		# insert list of tuples into db
+		mysql_hook.insert_rows(table = table_name, rows = rows)
 
 # define the default dag arguments
 default_args = {
@@ -77,20 +40,27 @@ dag = DAG(
 	schedule_interval = timedelta(minutes = 1440)
 	)
 
-# first task is to get the flight data from aviation stack
+# get the flight data from aviation stack
 task1 = BashOperator(
 	task_id = 'get_flight',
 	bash_command = 'python ~/airflow/dags/get_flight_data.py' ,
 	dag = dag
 	)
 
-# second task is to process the data and load into the database
-task2 =  PythonOperator(
+# get the airport and airline data from aviation stack
+task2 = BashOperator(
+	task_id = 'get_airport',
+	bash_command = 'python ~/airflow/dags/get_airport_data.py' ,
+	dag = dag
+	)
+
+# process and load into the database
+task3 =  PythonOperator(
 	task_id = 'transform_load',
 	provide_context = True,
 	python_callable = load_data,
 	dag = dag
 	)
 
-# task1 must be completed before task2 can start
-task1 >> task2
+# task hierarchy
+(task1, task2) >> task3
